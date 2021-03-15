cf988566c53ffc9a564bbdc29659ca603ebcd838
Tej-Birring <tejbirring@gmail.com>
2021-03-15 23:38:09 +0000

    config title correction

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Aut viam inveniam aut faciam',
-  titleTemplate: 'Aut viam inveniam aut faciam',
+  siteName: 'Aut Viam Inveniam Aut Faciam',
+  titleTemplate: 'Aut Viam Inveniam Aut Faciam',
   plugins: [],
   css: {
     loaderOptions: {
